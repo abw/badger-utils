bd73f01ae8b1cc20b35df376ce2923be22927e46
Andy Wardley <abw@wardley.org>
2024-12-26 13:07:14 +0000

    Changed build to vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts()
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: 'src/index.ts',
+      name: '@abw/badger-utils',
+      fileName: 'badger-utils'
+    }
+  },
   test: {
     include: ['test/**/*.[jt]s'],
     exclude: ['test/library/*.ts'],
