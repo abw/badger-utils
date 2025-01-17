4d641c84331adfdb27f959c4f37cba65335515d1
Andy Wardley <abw@wardley.org>
2025-01-17 18:43:43 +0000

    Updated lint

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['{src,test}/**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "indent": [
+        "error",
+        2,
+        {
+          "offsetTernaryExpressions": true,
+          "SwitchCase": 1
+        }
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "warn",
+        "single",
+        {
+          "avoidEscape": true,
+          "allowTemplateLiterals": true
+        }
+      ],
+      "semi": [
+        "error",
+        "never"
+      ]
+    },
+  },
+)
