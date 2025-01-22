18dbde68a2d3ec4e0598d42d5d2232ed858edd1d
Andy Wardley <abw@wardley.org>
2025-01-22 10:47:56 +0000

    Added vite-tsconfig-paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    dts()
+    tsconfigPaths(),
+    dts({
+      exclude: [
+        'test',
+        'website'
+      ],
+    })
   ],
   build: {
     sourcemap: true,
